67aefa7f20ca1d17769d81fe9f6d59925c99d1ec
Cristopher Barrios <cjrbarrios@gmail.com>
2019-05-24 21:56:35 -0600

    Relaciones

--- Connectar.py
+++ Connectar.py
@@ -1,18 +1,59 @@
+#Cristopher Barrios 18207
+#Documentacion https://neo4j-rest-client.readthedocs.io/en/latest/
 from neo4jrestclient.client import GraphDatabase
 gdb = GraphDatabase("http://localhost:7474", username="neo4j", password="CristopherBarrios")
 
+
+#insertando
 def agregarRestaurante(name):
     CadenaDeRestaurantes= gdb.nodes.create(nombre=name)
     CadenaDeRestaurantes.labels.add("CadenaDeRestaurantes")
+
+def agregarCliente(name,age):
+    Cliente= gdb.nodes.create(nombre=name,edad=age)
+    Cliente.labels.add("Cliente")
     
 def agregarEstilo(name,costo):
     EstilodeComida= gdb.nodes.create(nombre=name,precio=costo)
     EstilodeComida.labels.add("EstilodeComida")
     
-def agregarCliente(name,age):
-    Cliente= gdb.nodes.create(nombre=name,edad=age)
-    Cliente.labels.add("Cliente")
+def agregarPreferencia(name):
+    PreferenciaComida= gdb.nodes.create(nombre=name)
+    PreferenciaComida.labels.add("PreferenciaComida")
 
-def agregarPreferencia(name,age):
-    Cliente= gdb.nodes.create(nombre=name,edad=age)
-    Cliente.labels.add("Cliente")
+#Relaciones
+def clientePreferencia (name, preferencia):
+    Cliente= gdb.labels.get("Cliente")
+    Cliente.all()
+    PreferenciaComida=gdb.labels.get("PreferenciaComida")
+    PreferenciaComida.all()
+    Cliente.get(nombre=name)[0].relationships.create("prefiere",PreferenciaComida.get(nombre=preferencia)[0])
+
+def clienteEstilo (name, estilo):
+    Cliente= gdb.labels.get("Cliente")
+    Cliente.all()
+    EstilodeComida=gdb.labels.get("EstilodeComida")
+    EstilodeComida.all()
+    Cliente.get(nombre=name)[0].relationships.create("prefiere",EstilodeComida.get(nombre=estilo)[0])
+    
+def restaurantePreferencia (name, preferencia):
+    CadenaDeRestaurantes= gdb.labels.get("CadenaDeRestaurantes")
+    CadenaDeRestaurantes.all()
+    PreferenciaComida=gdb.labels.get("PreferenciaComida")
+    PreferenciaComida.all()
+    CadenaDeRestaurantes.get(nombre=name)[0].relationships.create("pertenece",PreferenciaComida.get(nombre=preferencia)[0])
+    
+def restauranteEstilo (name, estilo):
+    CadenaDeRestaurantes= gdb.labels.get("CadenaDeRestaurantes")
+    CadenaDeRestaurantes.all()
+    EstilodeComida=gdb.labels.get("EstilodeComida")
+    EstilodeComida.all()
+    CadenaDeRestaurantes.get(nombre=name)[0].relationships.create("petenece",EstilodeComida.get(nombre=estilo)[0])
+    
+
+#Fuciones para logaritmo
+def buscaruno(cliente, listaCliente):
+    a=-1
+    if cliente in listaCliente:
+        a=listaCliente.index(cliente)
+    return a
--- main.py
+++ main.py
@@ -1,26 +1,55 @@
-
+#Cristopher Jose Rodolfo Barrios Solis 18207
 #Proyecto 2 Algoritmos
+#Recomendar restaurantes
 from Connectar import *
 
-nombre = str(raw_input("Ingrese nombre de restaurante: "))
-est = str(raw_input("Ingrese nombre de Estilo: "))
 
-agregarEstilo(nombre,est)
-print("Bienvenido a nuestra aplicacion RESTAUNATOS\n")
-menu = input("Que desea hacer?\n\n"
+print("Bienvenido a nuestra aplicacion RESTAUNATOS ◕ ‿‿ ◕\n")
+menu = input("Que desea hacer?  (Ingrese numero)\n\n"
              "1. Comer Ya!\n"
              "2. Iniciar Sesion\n"
-             "3. Restaurante del dia\n"
-             "4. Salir\n\n")
+             "3. Administrador\n"
+             "4. Salir\n\n"
+             ": ")
 menu = str(menu)
 while (menu!="4"):
     if menu =="1":
-         a = input("Su restaurante sirve solo un tipo de comida?/n"
-                   "1. Si\n"
-                   "2. Quiza\n"
-                   "3. No se\n"
-                   "4. Casi no\n"
-                   "5. No\n\n")
+        nombre = str(raw_input("Ingrese su nombre: "))
+        edad = str(raw_input("Ingrese su edad: "))
+        agregarCliente(nombre,edad)
+
+        preguntaEstilo =str(input("Que estilo y precio de comida prefiere? (Ingrese numero)\n"
+                                   "1. Comida rapida, Accesible\n"
+                                   "2. Comida elegante, Alto\n"
+                                   "3. Comida saludable, Regular\n"
+                                   "4. Comida casera, Accesible\n\n"
+                                   ": "))
+        if preguntaEstilo=="1":
+            style = "Comida Rapida"
+        if preguntaEstilo=="2":
+            style = "Comida Elegante"
+        if preguntaEstilo=="3":
+            style = "Comida Saludable"
+        if preguntaEstilo=="4":
+            style = "Comida Casera"
+        clienteEstilo(nombre,style)
+        preguntaPreferencia =str(input("Que le da mas importancia a un restaurante? (Ingrese numero)\n"
+                                   "1. Ambiente del lugar\n"
+                                   "2. Sabor de la comida\n"
+                                   "3. Precio de la comida\n"
+                                   "4. Compania al ir a comer\n\n"
+                                   ": "))
+        if preguntaEstilo=="1":
+            preferencia = "Ambiente del lugar"
+        if preguntaEstilo=="2":
+            preferencia = "Sabor de la comida"
+        if preguntaEstilo=="3":
+            preferencia = "Precio de la comida"
+        if preguntaEstilo=="4":
+            preferencia = "Compania al ir a comer"
+        clientePreferencia(nombre,preferencia)
+        
+         
     if menu =="2":
         b = input("1. Iniciar Sesion\n"
                   "2. Registrarse\n\n")
@@ -38,7 +67,39 @@ while (menu!="4"):
             contrasena1 = str(contrasena1)
             contrasena1 = str(contrasena1)
     if menu =="3":
-        print("RESTAUNATOS te recomienta: \n")
+        nombreRestaurante = str(raw_input("Ingrese nombre de restaurante: "))
+        agregarRestaurante(nombreRestaurante)
+
+        preguntaEstilo =str(input("Que estilo y precio de comida prefiere? (Ingrese numero)\n"
+                                   "1. Comida rapida, Accesible\n"
+                                   "2. Comida elegante, Alto\n"
+                                   "3. Comida saludable, Regular\n"
+                                   "4. Comida casera, Accesible\n\n"
+                                   ": "))
+        if preguntaEstilo=="1":
+            styleRes = "Comida Rapida"
+        if preguntaEstilo=="2":
+            styleRes = "Comida Elegante"
+        if preguntaEstilo=="3":
+            styleRes = "Comida Saludable"
+        if preguntaEstilo=="4":
+            styleRes = "Comida Casera"
+        restauranteEstilo(nombreRestaurante,styleRes)
+        preguntaPreferencia =str(input("Que le da mas importancia a un restaurante? (Ingrese numero)\n"
+                                   "1. Ambiente del lugar\n"
+                                   "2. Sabor de la comida\n"
+                                   "3. Precio de la comida\n"
+                                   "4. Compania al ir a comer\n\n"
+                                   ": "))
+        if preguntaEstilo=="1":
+            preferenciaRes = "Ambiente del lugar"
+        if preguntaEstilo=="2":
+            preferenciaRes = "Sabor de la comida"
+        if preguntaEstilo=="3":
+            preferenciaRes = "Precio de la comida"
+        if preguntaEstilo=="4":
+            preferenciaRes = "Compania al ir a comer"
+        restaurantePreferencia(nombreRestaurante,preferenciaRes)
 print("Buen provecho!!!!")
         
     
